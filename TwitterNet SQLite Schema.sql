8e60e6b26c356ad21930937fb29e0c8172da891f
Adam Cooper <a.r.cooper@bolton.ac.uk>
2012-09-25 20:46:10 +0100

    added greylist flag

--- TwitterNet SQLite Schema.sql
+++ TwitterNet SQLite Schema.sql
@@ -2,6 +2,8 @@ CREATE TABLE "relation" (
     "follower_id" INTEGER NOT NULL,
     "followed_id" INTEGER NOT NULL
 );
+CREATE INDEX "FOLLOWER_ID_IDX" on relation (follower_id ASC);
+CREATE INDEX "FOLLOWED_ID_IDX" on relation (followed_id ASC);
 CREATE TABLE user (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -11,8 +13,6 @@ CREATE TABLE user (
     "cache_date" TEXT,
     "screen_name" TEXT NOT NULL,
     "description" TEXT
-);
-CREATE INDEX "FOLLOWER_ID_IDX" on relation (follower_id ASC);
-CREATE INDEX "FOLLOWED_ID_IDX" on relation (followed_id ASC);
+, "greylisted" INTEGER  NOT NULL  DEFAULT (0));
 CREATE UNIQUE INDEX "ID_IDX" on user (id ASC);
 CREATE INDEX "CACHE_DATE_IDX" on user (cache_date ASC);
