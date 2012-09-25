a8fd237437947babfc543280bf827ffbeaf0a422
Adam Cooper <a.r.cooper@bolton.ac.uk>
2012-09-25 18:10:04 +0100

    missed index for cache date query

--- TwitterNet SQLite Schema.sql
+++ TwitterNet SQLite Schema.sql
@@ -2,8 +2,6 @@ CREATE TABLE "relation" (
     "follower_id" INTEGER NOT NULL,
     "followed_id" INTEGER NOT NULL
 );
-CREATE INDEX "FOLLOWER_ID_IDX" on relation (follower_id ASC);
-CREATE INDEX "FOLLOWED_ID_IDX" on relation (followed_id ASC);
 CREATE TABLE user (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -14,4 +12,7 @@ CREATE TABLE user (
     "screen_name" TEXT NOT NULL,
     "description" TEXT
 );
+CREATE INDEX "FOLLOWER_ID_IDX" on relation (follower_id ASC);
+CREATE INDEX "FOLLOWED_ID_IDX" on relation (followed_id ASC);
 CREATE UNIQUE INDEX "ID_IDX" on user (id ASC);
+CREATE INDEX "CACHE_DATE_IDX" on user (cache_date ASC);
